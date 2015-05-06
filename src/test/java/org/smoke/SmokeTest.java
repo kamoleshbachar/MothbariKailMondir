af77eeccd0cdd2e5a0d408abb75659ce11fcf414
Kamolesh Bachar <kamolesh.bachar@yahoo.com>
2015-05-05 21:08:43 -0400

    Add Smoke test script

--- src/test/java/org/smoke/SmokeTest.java
+++ src/test/java/org/smoke/SmokeTest.java
@@ -1,5 +1,31 @@
 package org.smoke;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class SmokeTest {
+	WebDriver  driver = null;
+	
+	@BeforeMethod
+	public void beforeMethod(){
+		 driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void login() {
+		System.out.println(",kn");
+		driver.get("http://mothbarikalimondir.org/");
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "Moth Bari Kali Mondir");
+	}
+	@AfterMethod
+	public void afterMethod(){
+		driver.quit();
+		
+	}
 
 }
